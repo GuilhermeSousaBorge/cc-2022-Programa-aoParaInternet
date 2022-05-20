231b0ee6ef654aceb3f6726cf935c5345216aa77
GuilhermeSousaBorge <guilhermepllay157@gmail.com>
2022-05-20 16:56:03 -0300

    .

--- lista_5/Ex7/script.js
+++ lista_5/Ex7/script.js
@@ -1,9 +1,34 @@
+let nomes = ["Ana","bruna","jorge","enzo"]
+let numeros = [15,98,35,78]
+
 document.getElementById('addLinha').addEventListener('click', () => {
-    let tabela = document.getElementById("tabela").innerHTML
+    let indexNomes = Math.floor(Math.random() * nomes.length)
+    let indexNumeros = Math.floor(Math.random() * numeros.length)
+    let tabela = document.getElementById("tabela")
+    let linha = document.createElement("tr")
+    linha.setAttribute("class","line")
+    let dado = document.createElement("td")
+    let dado2 = document.createElement("td")
+    dado.appendChild(document.createTextNode(nomes[indexNomes]))
+    linha.appendChild(dado)
+    dado2.appendChild(document.createTextNode(numeros[indexNumeros]))
+    linha.appendChild(dado2)
+    tabela.appendChild(linha)
     console.log(tabela)
+    let lineTable = document.getElementsByClassName("line");
+    let value = lineTable.length
+    for (let line of lineTable) {
+        if (value % 2 === 0) {
+            line.style.backgroundColor = "white";
+        } else {
+            line.style.backgroundColor = "gray";
+        }
+        value++;
+    }
 })
 
-document.getElementById('remveLinha').addEventListener('click', () => {
+document.getElementById('removeLinha').addEventListener('click', () => {
     let tabela = document.getElementById("tabela")
-    tabela.removeChild(tabela.ariaRowCount.length)
+    console.log(tabela.lastChild)
+    tabela.removeChild(tabela.lastChild)
 })
